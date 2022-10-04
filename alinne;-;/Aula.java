586114bd54d64d998bc48f6bc3cc39e864a3b39a
asefrigg <111322170+aseFigg@users.noreply.github.com>
2022-10-03 21:32:56 -0300

    Add files via upload

---
+++ alinne;-;/Aula.java
@@ -0,0 +1,13 @@
+public class Aula{
+    public static void main(String[] args){
+
+        double valorA = 10.5;
+        double valorB = 10.8;
+
+        double soma = valorA + valorB;
+        System.out.println(soma);
+
+        double multiplicacao = valorA * valorB;
+        System.out.println(multiplicacao);
+    }
+}
